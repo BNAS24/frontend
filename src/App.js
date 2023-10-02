25b78f85fa35b5befc9189ebcf89b9c0dfe15ec4
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2023-10-02 15:57:42 -0400

    added pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { Homepage } from './pages/homepage';
+import { LoginRegister } from './pages/login_register';
 import './App.css';
 
 function App() {
   return (
-    <>
-          <Homepage />
-    </>
-
+    <Routes>
+      <Route path='/' element={<Homepage />} />
+      <Route path='/Login_Register' element={<LoginRegister />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/login_register.js
@@ -0,0 +1,11 @@
+import './login_register'
+
+export const LoginRegister = () => {
+    return (
+        <>
+        <div className='main-container'>
+        <img src="https://i.postimg.cc/wjkpvXx7/fans.png" alt="logo" className="login-logo"/>
+        </div>
+        </>
+    );
+}
