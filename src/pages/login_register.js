ae32cd3b14bbb7fa463b796549362e117632ba6f
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2023-10-02 16:07:18 -0400

    css updates

--- src/pages/login_register.js
+++ src/pages/login_register.js
@@ -2,10 +2,9 @@ import '../styles/login_register.css'
 
 export const LoginRegister = () => {
     return (
-        <>
-        <div className='main-container'>
-        <img src="https://i.postimg.cc/wjkpvXx7/fans.png" alt="logo" className="login-logo"/>
-        </div>
-        </>
+            <div className='main-container'>
+                <img src="https://i.postimg.cc/wjkpvXx7/fans.png" alt="logo" className="login-logo" />
+                <p className='login-title'>Login to continue</p>
+            </div>
     );
 }
