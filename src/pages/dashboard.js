fe4be0722c44eb22ece7b80fbd61adff8d842c37
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2023-10-04 17:07:28 -0400

    ...

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -6,6 +6,8 @@ import { NavBar } from '../components/authnav';
 import { Footer } from '../components/authfoot';
 import { Avatar, Grid, Typography } from '@mui/material';
 
+const fakeNotifications = ['John Doe', 'Chief Keef', 'Pex Ranger', 'Golden Glove', 'Axel Ranger', 'Free Woo', '99 Havoc', 'Jupiter15', 'iloveTheEagles', 'xasdf', '1942$hots', 'Damn Daniel', 'Moment4life', 'Gwillikers', 'Spidyman', 'JumpmanJumpman', 'What else']
+
 export const Dashboard = () => {
     return (
         <ThemeProvider theme={customTheme}>
@@ -24,12 +26,14 @@ export const Dashboard = () => {
                         flexGrow: '1',
                     }}
                 >
+                    {/*1st Grid Item*/}
                     <Grid item xs={12} sm={12} md={12} lg={12} sx={{ gridArea: '1 / 1 / 2 / 2' }}>
                         <Container sx={{ display: 'flex', flexDirection: 'row', height: '100%', alignItems: 'center', paddingLeft: '0 !important', color: 'white' }}>
                             <Avatar variant='square' alt='profile picture' src='https://i.postimg.cc/qq6K81pw/Profile-Picture-Placeholder.png' sx={{ height: '104px', width: '104px' }} />
                             <Typography style={{ marginLeft: '24px' }} variant='h3'>Hello Brandon! {/*The name will be dynamicall rendered*/}</Typography>
                         </Container>
                     </Grid>
+                    {/*2nd Grid Item*/}
                     <Grid item xs={12} sm={12} md={12} lg={12} sx={{ gridArea: '2 / 1 / 3 / 2' }}>
                         <Container sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', paddingLeft: '0 !important', paddingRight: '0 !important', gap: '24px', height: '100%', color: 'white' }}>
 
@@ -43,13 +47,19 @@ export const Dashboard = () => {
 
                             <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', paddingLeft: '0 !important', paddingRight: '0 !important', height: '100%', width: '100%' }}>
                                 <Typography noWrap variant='h5'>Notifications</Typography>
-                                
-                                <Container sx={{ height: '100%', width: '100%', marginTop: '24px', border: 'solid 1px white' }}>
 
+                                <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%', overflowY: 'scroll', width: '100%', marginTop: '24px', border: 'solid 1px white' }}>
+                                    {fakeNotifications.map((notification, index) =>
+                                        <Typography
+                                            variant='body1'
+                                            noWrap
+                                            sx={{ marginTop: '24px' }}
+                                        >{notification} liked your post</Typography>)}
                                 </Container>
                             </Container>
                         </Container>
                     </Grid>
+                    {/*3rd Grid Item*/}
                     <Grid item xs={12} sm={12} md={12} lg={12} sx={{ background: 'linear-gradient(to bottom, #0081D4, #ffffff)', border: 'solid 1px green', gridArea: '1 / 2 / 3 / 3' }}>
 
                     </Grid>
