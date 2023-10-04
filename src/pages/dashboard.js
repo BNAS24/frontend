5c212cce297b5784a276d316489a35e47c78ad1d
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2023-10-04 13:24:39 -0400

    ...

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,10 +1,10 @@
 import '../styles/dashboard.css';
 import customTheme from '../styles/context/customtheme';
 import { ThemeProvider } from '@emotion/react';
-import { Box } from '@mui/system';
+import { Box, Container } from '@mui/system';
 import { NavBar } from '../components/authnav';
 import { Footer } from '../components/authfoot';
-import { Grid } from '@mui/material';
+import { Avatar, Grid, Typography } from '@mui/material';
 
 export const Dashboard = () => {
     return (
@@ -19,16 +19,23 @@ export const Dashboard = () => {
                         gridTemplateColumns: 'repeat(2, 1fr)',
                         gridTemplateRows: '1fr 3fr',
                         gridGap: '8px',
+                        columnGap: '56px',
                         padding: '24px 0',
                         border: 'dashed 1px lime',
                         flexGrow: '1',
                     }}
                 >
                     <Grid item xs={12} sm={12} md={12} lg={12} sx={{ border: 'dashed 1px red', gridArea: '1 / 1 / 2 / 2' }}>
-                        <img src='https://i.postimg.cc/qq6K81pw/Profile-Picture-Placeholder.png' alt='profile-ph'></img>
+                        <Container sx={{ display: 'flex', flexDirection: 'row', height: '100%', color: 'white', alignItems: 'center', border: 'dashed 1px yellow' }}>
+                            <Avatar variant='square' alt='profile picture' src='https://i.postimg.cc/qq6K81pw/Profile-Picture-Placeholder.png' sx={{ height: '104px', width: '104px' }} />
+                            <Typography style={{ marginLeft: '8px' }} variant='h5'>Hello Brandon! {/*The name will be dynamicall rendered*/}</Typography>
+                        </Container>
                     </Grid>
-                    <Grid item xs={12} sm={12} md={12} lg={12} sx={{ border: 'dashed 1px red', gridArea: '2 / 1 / 3 / 2' }}>
-                        <Box color='white'>2</Box>
+                    <Grid item xs={12} sm={12} md={12} lg={12} sx={{ border: 'dashed 1px red', gridArea: '2 / 1 / 3 / 2', padding: '8px' }}>
+                        <Container sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', gap: '40px', height: '100%', color: 'white', border: 'dashed 1px lime' }}>
+                            <Container sx={{ height: '100%', width: '50%', border: 'dashed 1px red' }}>12</Container>
+                            <Container sx={{ width: '50%', border: 'dashed 1px red' }}>11</Container>
+                        </Container>
                     </Grid>
                     <Grid item xs={12} sm={12} md={12} lg={12} sx={{ border: 'dashed 1px red', gridArea: '1 / 2 / 3 / 3' }}>
                         <Box color='white'>3</Box>
