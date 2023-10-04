19ae4db74e012f6e7ffeb669e819c6782acd92d3
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2023-10-04 11:08:09 -0400

    ...

--- src/components/authnav.js
+++ src/components/authnav.js
@@ -1,1 +1,20 @@
 //This will be a nav component that you can reuse for your pages.
+import { NavLink } from "react-router-dom"
+import customTheme from "../styles/context/customtheme"
+
+export const NavBar = () => {
+    return (
+        <nav className='header-container'>
+            <div className='logo_name_container'>
+                <img src='https://i.postimg.cc/wjkpvXx7/fans.png' className='sports-fanatic-logo' alt='logo' />
+                <div className='title'>Sports Fanatic</div>
+            </div>
+            <div className='nav-container'>
+                <p><NavLink className='homepage-links-hover-state' to='/dashboard' style={({ isActive }) => ({ color: isActive ? customTheme.palette.secondary.main : null })}>Dashboard</NavLink></p>
+                <p><NavLink className='homepage-links-hover-state' to='/forums' style={({ isActive }) => ({ color: isActive ? customTheme.palette.secondary.main : null })}>Forums</NavLink></p>
+                <p><NavLink className='homepage-links-hover-state' to='/live-scores' style={({ isActive }) => ({ color: isActive ? customTheme.palette.secondary.main : null })}>Live Scores</NavLink></p>
+                <p><NavLink className='homepage-links-hover-state' to='/profile' style={({ isActive }) => ({ color: isActive ? customTheme.palette.secondary.main : null })}>Profile</NavLink></p>
+            </div>
+        </nav>
+    )
+}
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -2,26 +2,17 @@ import '../styles/dashboard.css';
 import { NavLink } from 'react-router-dom';
 import customTheme from '../styles/context/customtheme';
 import { ThemeProvider } from '@emotion/react';
+import { Box } from '@mui/system';
+import { NavBar } from '../components/authnav';
 
 
 export const Dashboard = () => {
     return (
         <ThemeProvider theme={customTheme}>
-            <div className='wrapper'>
-                {/*Navbar*/}
-                <nav className='header-container'>
-                    <div className='logo_name_container'>
-                        <img src='https://i.postimg.cc/wjkpvXx7/fans.png' className='sports-fanatic-logo' alt='logo' />
-                        <div className='title'>Sports Fanatic</div>
-                    </div>
-                    <div className='nav-container'>
-                        <p><NavLink className='homepage-links-hover-state' to='/dashboard' style={({ isActive }) => ({ color: isActive ? customTheme.palette.secondary.main : null })}>Dashboard</NavLink></p>
-                        <p><NavLink className='homepage-links-hover-state' to='/forums' style={({ isActive }) => ({ color: isActive ? customTheme.palette.secondary.main : null })}>Forums</NavLink></p>
-                        <p><NavLink className='homepage-links-hover-state' to='/live-scores' style={({ isActive }) => ({ color: isActive ? customTheme.palette.secondary.main : null})}>Live Scores</NavLink></p>
-                        <p><NavLink className='homepage-links-hover-state' to='/login'>Login</NavLink></p>
-                    </div>
-                </nav>
+            <Box className='wrapper' sx={{ display: 'flex', flexDirection: 'column', position: '100vh', border: 'dashed 1px yellow' }}>
 
+                {/*Navbar*/}
+                <NavBar />
                 {/*Content goes below this line*/}
 
 
@@ -33,7 +24,8 @@ export const Dashboard = () => {
                         <p className='homepage-links-hover-state'>Contact Us</p>
                     </div>
                 </footer>
-            </div>
+
+            </Box>
         </ThemeProvider>
     );
 }
