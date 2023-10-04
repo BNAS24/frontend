cdd48f6f04df7009db6a7b66dd95cd804dc5c395
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2023-10-03 23:41:04 -0400

    ...

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -15,24 +15,24 @@ export const Dashboard = () => {
                         <div className='title'>Sports Fanatic</div>
                     </div>
                     <div className='nav-container'>
-                        <p><NavLink className='homepage-links-hover-state' to='/dashboard' style={{ color: customTheme.palette.secondary.main }}>Dashboard</NavLink></p>
-                        <p><NavLink className='homepage-links-hover-state' to='/forums' style={{ color: customTheme.palette.secondary.main }}>Forums</NavLink></p>
-                        <p><NavLink className='homepage-links-hover-state' to='/live-scores' style={{ color: customTheme.palette.secondary.main }}>Live Scores</NavLink></p>
+                        <p><NavLink className='homepage-links-hover-state' to='/dashboard' style={({ isActive }) => ({ color: isActive ? customTheme.palette.secondary.main : null })}>Dashboard</NavLink></p>
+                        <p><NavLink className='homepage-links-hover-state' to='/forums' style={({ isActive }) => ({ color: isActive ? customTheme.palette.secondary.main : null })}>Forums</NavLink></p>
+                        <p><NavLink className='homepage-links-hover-state' to='/live-scores' style={({ isActive }) => ({ color: isActive ? customTheme.palette.secondary.main : null})}>Live Scores</NavLink></p>
                         <p><NavLink className='homepage-links-hover-state' to='/login'>Login</NavLink></p>
                     </div>
                 </nav>
 
-                {/*Content*/}
+                {/*Content goes below this line*/}
 
 
                 {/*Footer*/}
-                            <footer className='footer-container'>
-                <p className='copyright'>© 2023 Copyright  Sports Fanatic Hub</p>
-                <div className='nav-container'>
-                    <p className='homepage-links-hover-state'>About Us</p>
-                    <p className='homepage-links-hover-state'>Contact Us</p>
-                </div>
-            </footer>
+                <footer className='footer-container'>
+                    <p className='copyright'>© 2023 Copyright  Sports Fanatic Hub</p>
+                    <div className='nav-container'>
+                        <p className='homepage-links-hover-state'>About Us</p>
+                        <p className='homepage-links-hover-state'>Contact Us</p>
+                    </div>
+                </footer>
             </div>
         </ThemeProvider>
     );
