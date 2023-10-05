78c521aef73b9ab8904deb7d12226c0590a8c77a
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2023-10-04 22:32:25 -0400

    ...

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -30,7 +30,7 @@ export const Dashboard = () => {
                     {/*1st Grid Item*/}
                     <Grid item xs={12} sm={12} md={12} lg={12} sx={{ gridArea: '1 / 1 / 2 / 2', overflow: 'hidden' }}>
                         <Container sx={{ display: 'flex', flexDirection: 'row', height: '100%', alignItems: 'center', paddingLeft: '0 !important', color: 'white' }}>
-                            <Avatar variant='square' alt='profile picture' src='https://i.postimg.cc/qq6K81pw/Profile-Picture-Placeholder.png' sx={{ height: '104px', width: '104px' }} />
+                            <Avatar variant='square' alt='profile picture' src='https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png' sx={{ height: '104px', width: '104px' }} />
                             <Typography style={{ marginLeft: '24px' }} variant='h3'>Hello Brandon! {/*The name will be dynamicall rendered*/}</Typography>
                         </Container>
                     </Grid>
