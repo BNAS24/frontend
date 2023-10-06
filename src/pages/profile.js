b81c62eb94d51ec61cdbaedc67fba4723b212f45
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2023-10-06 05:39:01 -0400

    ...

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,14 +1,64 @@
 import '../styles/profile.css'
-import { Box } from '@mui/system';
+import { Box, Container } from '@mui/system';
 import { NavBar } from "../components/authnav"
 import { Footer } from '../components/authfoot';
 import { Grid } from '@mui/material';
 
 export const Profile = () => {
     return (
-        <Box>
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                minHeight: '100%',
+                width: '100%',
+                border: 'dashed 1px yellow'
+            }}>
             <NavBar />
-<Grid>Hello World</Grid>
+            <Grid
+                container
+                sx={{
+                    display: 'grid',
+                    gridTemplateColumns: 'repeat(2, 1fr)',
+                    gridTemplateRows: '1fr 5fr',
+                    gridGap: '8px',
+                    columnGap: '56px',
+                    padding: '24px 24px',
+                    flex: '1',
+                    maxHeight: '100vh',
+                }}
+            >
+                {/*First Grid Item*/}
+                <Grid
+                    item
+                    xs={12} sm={12} md={12} lg={12}
+                    sx={{
+                        gridArea: '1 / 1 / 2 / 2',
+                    }}>
+                
+                    1
+                </Grid>
+                {/*Second Grid Item*/}
+                <Grid
+                    item
+                    xs={12} sm={12} md={12} lg={12}
+                    sx={{
+                        gridArea: '2 / 1 / 3 / 2',
+                    }}>
+
+                    2
+                </Grid>
+                {/*Third Grid Item*/}
+                <Grid
+                    item
+                    xs={12} sm={12} md={12} lg={12}
+                    sx={{
+                        gridArea: '1 / 2 / 3 / 3',
+                    }}
+                >
+                    3
+                </Grid>
+            </Grid>
             <Footer />
         </Box>
     )
