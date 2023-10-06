1f83d7ab8d410de04809abe2accaf4fc42746154
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2023-10-06 19:45:33 -0400

    ..

---
+++ src/components/commentmd.js
@@ -0,0 +1,53 @@
+import { Modal, Box, Typography, Button } from '@mui/material';
+
+export const CommentsModal = ({ open, onClose, username, comment }) => {
+
+
+    return (
+        <Modal
+            open={open}
+            onClose={onClose}
+        >
+            <Box
+                sx={{
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    bgcolor: 'var(--theme-blue)',
+                    border: 'solid 2px var(--theme-orange)',
+                    p: 2,
+                    width: '50%',
+                    minWidth: '300px',
+                    height: '50%',
+                }}
+            >
+                <Typography
+                    variant="h6"
+                >
+                    {username}
+                </Typography>
+                <Typography
+                    variant="body2"
+                >
+                    {comment}
+                </Typography>
+                <Button
+                    variant='outlined'
+                    onClick={onClose}
+                    sx={{
+                        color: 'var(--theme-orange)',
+
+                        '&:hover': {
+                            border: '1px solid var(--theme-orange)',
+                            color: 'var(--theme-orange)',
+                        },
+                    }}
+                >
+                    Close
+                </Button>
+
+            </Box>
+        </Modal>
+    )
+}
