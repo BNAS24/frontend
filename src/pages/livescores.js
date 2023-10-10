e1747bafb5bfe498f940c2af4a5e1985f567f404
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2023-10-10 10:42:37 -0400

    minor state enhancements

--- src/pages/livescores.js
+++ src/pages/livescores.js
@@ -1,8 +1,16 @@
+import '../styles/livescores.css'
 import { NavBar } from '../components/authnav'
 import { Footer } from '../components/authfoot'
+import { useState } from 'react'
 import { Container } from '@mui/system'
+import { Typography } from '@mui/material'
+
+const sports = ['NFL', 'NBA', 'NHL', 'MLB', 'Soccer']
 
 export const LiveScores = () => {
+
+    const [sportSelected, setSportSelected] = useState(null)
+
     return (
         <Container
             maxWidth='xlg'
@@ -22,11 +30,63 @@ export const LiveScores = () => {
                     flex: '1',
                     display: 'flex',
                     flexDirection: 'row',
-                    justifyContent: 'center'
+                    justifyContent: 'center',
+                    height: '100%',
+                    border: 'solid 1px red'
                 }}
             >
-                <Container>1</Container>
-                <Container>2</Container>
+                <Container
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'flex-start',
+                        border: 'dashed 1px yellow'
+                    }}
+                >
+                    <Typography
+                        variant='h3'
+                        align='center'
+                        sx={{
+                            marginTop: '24px'
+                        }}
+                    >
+                        Sports Categories
+                    </Typography>
+                    <Container
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            justifyContent: 'space-evenly',
+                            height: '100%',
+                            border: 'dashed 1px yellow'
+                        }}
+                    >
+                        {
+                            Object.keys(sports).map((key) =>
+                                <Typography
+                                    key={key}
+                                    onClick={() => { setSportSelected(key) }}
+                                    className={sportSelected === key ? 'links-hover-state sport-selected-active-state' : 'links-hover-state'}
+                                // className='links-hover-state'
+                                >
+                                    {sports[key]}
+                                </Typography>
+                            )
+                        }
+                    </Container>
+                </Container>
+                <Container
+                    sx={{
+                        display: 'flex',
+                        color: 'white',
+                        justifyContent: 'center',
+                        border: 'dashed 1px yellow'
+                    }}
+                >
+                    2
+                </Container>
             </Container>
             <Footer />
         </Container>
