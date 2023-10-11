72913aa6536e3c55c98b21565c1d62f5f071536a
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2023-10-11 17:39:23 -0400

    ...

---
+++ src/datastore/aboutus.js
@@ -0,0 +1,22 @@
+export const aboutUs = {
+    aboutUs: {
+        title: 'About Us',
+        body: `Welcome to Sports Fanatic Hub, where passion for sports meets the power of community. We're more than just a website; we're a gathering place for fans from all corners of the sports universe. Our journey began with a simple idea: to create a space where sports enthusiasts could come together to celebrate their love for the game.`
+    },
+    ourMission: {
+        title: 'Our Mission',
+        body: `At Sports Fanatic Hub, our mission is clear—to connect fans, ignite discussions, and elevate the sports experience for all. We believe that sports have the unique ability to transcend boundaries, unite people, and inspire greatness. Our platform is built on the belief that every fan deserves a voice and a vibrant community to share their passion.`
+    },
+    setsUsApart: {
+        title: 'What Sets Us Apart',
+        body: `What makes Sports Fanatic Hub special is you—the devoted fans who bring the energy, the insights, and the unwavering spirit of sportsmanship. Whether you're a die-hard supporter of your hometown team, a stats guru, or just looking for some friendly banter, you'll find your place here.`
+    },
+    communityDriven: {
+        title: 'Community Driven',
+        body: `We're proud to be a community-driven platform. Our discussion forums, live game tracking, and user-generated content are fueled by your enthusiasm. It's your discussions, your debates, and your camaraderie that make Sports Fanatic Hub a hub like no other.`
+    },
+    joinUsToday: {
+        title: 'Join Us Today',
+        body: `So, if you're ready to dive into the heart of sports, connect with fellow fans, and share your perspectives, there's no better time to join Sports Fanatic Hub. Together, we'll celebrate victories, analyze defeats, and revel in the excitement of the game we all adore.`
+    }
+}
--- src/pages/aboutus.js
+++ src/pages/aboutus.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { NavBar } from '../components/authnav'
 import { Footer } from '../components/authfoot'
 import { Container } from '@mui/system'
+import { aboutUs } from '../datastore/aboutus'
 
 export const AboutUs = () => {
     return (
@@ -26,6 +27,7 @@ export const AboutUs = () => {
                     display: 'flex',
                     flexDirection: 'row',
                     justifyContent: 'center',
+                    gap: '40px',
                     height: '100%',
                     width: '100%',
                     paddingLeft: '0 !important',
@@ -33,7 +35,28 @@ export const AboutUs = () => {
                     border: 'solid 1px red'
                 }}
             >
-                Hello
+                <Container
+                maxWidth='100%'
+                    sx={{
+                        color: 'white',
+                        border: 'dashed 1px yellow'
+                    }}
+                >
+                    1
+                </Container>
+                <Container
+                maxWidth='100%'
+                    sx={{
+                        color: 'white',
+                        border: 'dashed 1px yellow',
+                        backgroundImage: 'url(https://i.postimg.cc/wvjXZ8Tg/Basketball-Action-Shot.jpg)',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundSize: 'contain',
+                        backgroundPosition: 'center',
+                    }}
+                >
+                    2
+                </Container>
             </Container>
             <Footer />
         </Container>
