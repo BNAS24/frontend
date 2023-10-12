da8cb1102177ef72c5c415de1b7b6df5edf0ae5c
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2023-10-12 11:57:05 -0400

    ...

--- src/datastore/contactus.js
+++ src/datastore/contactus.js
@@ -1,6 +1,15 @@
+import React from "react";
+
+
+const email = (
+<a href='mailto:contact@sportsfanatichub.com'>
+contact@sportsfanatichub.com
+</a>
+)
+
 const contactUs = {
     title: 'Contact Us',
-    body: `At Sports Fanatic Hub, we value your feedback, questions, and suggestions. If you'd like to get in touch with us, please feel free to send us an email at contact@sportsfanatichub.com.`
+    body: `At Sports Fanatic Hub, we value your feedback, questions, and suggestions. If you'd like to get in touch with us, please feel free to send us an email at ${email}.`
 }
 
 export default contactUs;
