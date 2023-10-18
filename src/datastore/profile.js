5f45358a39500b57722c1716d8185bec00cd9f13
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2023-10-18 18:52:55 -0400

    ...

---
+++ src/datastore/profile.js
@@ -0,0 +1,43 @@
+export const badges = ['Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Master', 'Grandmaster'];
+
+export const settings = {
+    changeEmailAddress: {
+        title: 'Change Email Address',
+        oldEmailPrompt: 'Old Email',
+        newEmailPrompt: 'New Email',
+        confirmNewEmailPrompt: 'Confirm New Email',
+        confirmEmailPrompt: 'Confirm Email',
+    },
+    changePassword: {
+        title: 'Change Password',
+        oldPasswordPrompt: 'Old Password',
+        newPasswordPrompt: 'New Password',
+        confirmPasswordPrompt: 'Confirm New Password',
+        buttonText: 'Update Password'
+    },
+    updateProfilePicter: {
+        title: 'Update Profile Picture',
+        currentProfilePicFile: 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png',
+        buttonText: 'Upload Image'
+    },
+    userStats: {
+        title: 'User Stats',
+        followers: {
+            title: 'Followers',
+            count: 2045
+        },
+        following: {
+            title: 'Following',
+            count: 235
+        },
+        numberOfPosts: {
+            title: 'Number of Posts',
+            count: 756
+        }
+    },
+    logout: {
+        title: 'Logout',
+        logoutWarning: 'Are you sure you want to logout?',
+        buttonText: 'Logout'
+    }
+}
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -7,50 +7,7 @@ import { SettingsModal } from '../components/settingsmd';
 import '../styles/profile.css';
 import { useSidebar } from '../context/mobilenav';
 import { SideBarNav } from '../components/helpers/sidebarnav';
-
-const badges = ['Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Master', 'Grandmaster'];
-
-const settings = {
-    changeEmailAddress: {
-        title: 'Change Email Address',
-        oldEmailPrompt: 'Old Email',
-        newEmailPrompt: 'New Email',
-        confirmNewEmailPrompt: 'Confirm New Email',
-        confirmEmailPrompt: 'Confirm Email',
-    },
-    changePassword: {
-        title: 'Change Password',
-        oldPasswordPrompt: 'Old Password',
-        newPasswordPrompt: 'New Password',
-        confirmPasswordPrompt: 'Confirm New Password',
-        buttonText: 'Update Password'
-    },
-    updateProfilePicter: {
-        title: 'Update Profile Picture',
-        currentProfilePicFile: 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png',
-        buttonText: 'Upload Image'
-    },
-    userStats: {
-        title: 'User Stats',
-        followers: {
-            title: 'Followers',
-            count: 2045
-        },
-        following: {
-            title: 'Following',
-            count: 235
-        },
-        numberOfPosts: {
-            title: 'Number of Posts',
-            count: 756
-        }
-    },
-    logout: {
-        title: 'Logout',
-        logoutWarning: 'Are you sure you want to logout?',
-        buttonText: 'Logout'
-    }
-}
+import { badges, settings } from '../datastore/profile';
 
 export const Profile = () => {
 
