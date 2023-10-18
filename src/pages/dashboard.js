4f84aaaf8ebb220ee9035fb95a12c2524459cf9d
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2023-10-18 11:00:22 -0400

    ...

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -11,46 +11,7 @@ import { NavBar } from '../components/authnav';
 import { CommentsModal } from '../components/commentmd';
 import customTheme from '../styles/context/customtheme';
 import '../styles/dashboard.css';
-
-
-const fakeTeams = ['Philadelphia Eagles', 'Phillies', ' 76ixers', 'Union', 'Flyers', 'Norristown Bandits', 'New York Yankees', 'Los Angeles Lakers', 'Dallas Cowboys', 'Boston Red Sox', 'Chicago Bulls', 'Green Bay Packers', 'Golden State Warriors', 'Pittsburgh Steelers', 'San Francisco Giants', 'New England Patriots']
-
-const fakeNotifications = ['John Doe', 'Chief Keef', 'Pex Ranger', 'Sally', 'Jeff100', 'xrel12', '1234Hey', 'iloveFootball', 'Eaglesrule', 'SuperCoder123', 'TechExplorer24', 'StarGazer99', 'PexRanger16', 'AdventureSeeker77', 'MusicMaestro55', 'FitnessFanatic19', 'TravelEnthusiast7', 'FoodieDelight', 'GamingNinja21', 'MovieBuff123']
-
-const postContent = {
-    user1: {
-        username: 'Username123',
-        timeCreated: '1hr ago',
-        postText: 'Hey everybody! I love sports and I think the Philadelphia Eagles are the best team in the NFL!',
-        forumLink: '/forums'
-    },
-    user2: {
-        username: 'JupiterSkiees0',
-        timeCreated: '1m ago',
-        postText: 'New england needs to bring back Tom Brady!',
-        forumLink: '/'
-    },
-    user3: {
-        username: 'SoccerFanatic',
-        timeCreated: '2h ago',
-        postText: 'I think soccer is the best sport in the world!',
-        forumLink: '/forums'
-    },
-    user4: {
-        username: 'BasketballLover',
-        timeCreated: '3h ago',
-        postText: 'LeBron James is the greatest basketball player of all time!',
-        forumLink: '/forums'
-    },
-    user5: {
-        username: 'tinyFootball',
-        timeCreated: '4h ago',
-        postText: 'I cant wait for the next football season to start!',
-        forumLink: '/forums'
-    }
-};
-
-
+import { fakeTeams, fakeNotifications, postContent } from '../datastore/dashboard';
 
 export const Dashboard = () => {
 
