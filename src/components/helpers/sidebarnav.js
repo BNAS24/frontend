47581c05cbf97a5f2bae0b55724171e6574a1907
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2023-10-19 10:49:12 -0400

    ...

--- src/components/helpers/sidebarnav.js
+++ src/components/helpers/sidebarnav.js
@@ -20,12 +20,29 @@ export const SideBarNav = () => {
                 <div
                     style={{
                         display: 'flex',
+                        flexDirection: 'column',
                     }}
                 >
-                    <p><NavLink className='links-hover-state' to='/dashboard' style={({ isActive }) => ({ color: isActive ? customTheme.palette.secondary.main : null })}>Dashboard</NavLink></p>
-                    <p><NavLink className='links-hover-state' to='/forums' style={({ isActive }) => ({ color: isActive ? customTheme.palette.secondary.main : null })}>Forums</NavLink></p>
-                    <p><NavLink className='links-hover-state' to='/live-scores' style={({ isActive }) => ({ color: isActive ? customTheme.palette.secondary.main : null })}>Live Scores</NavLink></p>
-                    <p><NavLink className='links-hover-state' to='/profile' style={({ isActive }) => ({ color: isActive ? customTheme.palette.secondary.main : null })}>Profile</NavLink></p>
+                    <p>
+                        <NavLink className='links-hover-state' to='/dashboard' style={({ isActive }) => ({ color: isActive ? customTheme.palette.secondary.main : null })}>
+                            Dashboard
+                        </NavLink>
+                    </p>
+                    <p>
+                        <NavLink className='links-hover-state' to='/forums' style={({ isActive }) => ({ color: isActive ? customTheme.palette.secondary.main : null })}>
+                            Forums
+                        </NavLink>
+                    </p>
+                    <p>
+                        <NavLink className='links-hover-state' to='/live-scores' style={({ isActive }) => ({ color: isActive ? customTheme.palette.secondary.main : null })}>
+                            Live Scores
+                        </NavLink>
+                    </p>
+                    <p>
+                        <NavLink className='links-hover-state' to='/profile' style={({ isActive }) => ({ color: isActive ? customTheme.palette.secondary.main : null })}>
+                            Profile
+                        </NavLink>
+                    </p>
                 </div>
             </div>
             <div
